1f21dcf5a359ad2e5224b25cfa50a6c6a60f4003
Alimence <giranezaalmence@gmail.com>
2025-03-17 20:09:08 +0200

    Feat:Ending recipe application

---
+++ version_1/script2.js
@@ -0,0 +1,188 @@
+let recipes = [];
+let oldRecipes =[];
+
+const displayRecipes = () => {
+    const recipeList = document.querySelector('#recipeList')
+    
+
+
+if(recipeList){
+    recipeList.innerHTML = "";
+    recipes.forEach((recipe, index) => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add("bg-white", "p-4", "rounded", "shadow", "mb-4")
+        recipeCard.innerHTML = `
+        <h2 class = "text-lg font-bold" id = "titleDisplay-${index}">${recipe.title}</h2>
+        <input type = "text" id = "titleInput-${index}" class = "hidden border p-2 w-full mb-2 rounded-lg" value = "${recipe.title}">
+
+        <p class = "text-sm text-gray-500" id = "ingredientsDisplay-${index}" ><strong class = "font-bold text-lg">Ingredients: &ensp;</strong>${recipe.ingredients}</p>
+        <textarea id = "ingredientsInput-${index}" class = "hidden border p-2 w-full mb-2 rounded-lg">${recipe.ingredients}</textarea>
+
+         <p class = "text-sm text-gray-500" id = "stepsDisplay-${index}" ><strong class = "font-bold text-lg">Steps: &ensp;</strong>${recipe.steps}</p>
+<textarea id = "stepsInput-${index}" class = "hidden border p-2 w-full mb-2 rounded-lg">${recipe.steps}</textarea>
+
+
+         <button class = "bg-green-500 text-white px-2 py-1 rounded mt-2" id = "editBtn-${index}" onClick = "editRecipe(${index})">Edit</button>
+
+         <button class = "bg-red-500 text-white px-2 py-1 rounded mt-2" id = "deleteBtn-${index}" onClick = "deleteRecipe(${index})">Delete</button>
+
+         <button class = "bg-green-500 text-white px-2 py-1 rounded mt-2 hidden" id = "saveBtn-${index}" onClick = "saveRecipe(${index})">Save</button>
+
+<button class = "bg-gray-500 text-white px-2 py-1 rounded mt-2 hidden" id = "cancelBtn-${index}" onClick = "cancelEdit(${index})">Cancel</button>
+
+        `;
+        recipeList.appendChild(recipeCard);
+    } )
+}
+}
+const saveRecipeToLocalStorage = () => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+    localStorage.setItem("oldRecipes", JSON.stringify(oldRecipes))
+}
+
+const loadRecipesFromLocalStorage = () => {
+    const storedRecipes = localStorage.getItem("recipes");
+    const storedOldRecipes = localStorage.getItem("oldRecipes");
+
+    if(storedOldRecipes){
+        oldRecipes = JSON.parse(storedOldRecipes)
+    }
+    
+    if(storedRecipes){
+        recipes = JSON.parse(storedRecipes);
+    }
+}
+
+const showError = (elementId, message) => {
+    const errorElement = document.getElementById(elementId)
+    if(errorElement){
+        errorElement.innerText = message ;
+    errorElement.classList.remove("hidden");
+    }
+    
+}
+const hideError = (elementId) => {
+    const errorElement = document.getElementById(elementId);
+    errorElement.classList.add("hidden");
+}
+const addRecipe = (event) => {
+    event.preventDefault();
+    const recipeTitle = document.getElementById("recipeTitle").value.trim();
+    const recipeIngredients = document.getElementById("recipeIngredients").value.trim();
+    const recipeSteps = document.getElementById("recipeSteps").value.trim();
+
+    
+    hideError("titleError");
+    hideError("ingredientsError");
+    hideError("stepsError");
+
+    let isValid = true;
+
+    if(recipeTitle === ""){
+        showError("titleError", "Please enter the Recipe title!!");
+        isValid = false;
+    }
+    if(recipeIngredients === ""){
+        showError("ingredientsError", "Please enter the Recipe ingredients!!");
+        isValid = false;
+    }
+    if(recipeSteps === ""){
+        showError("stepsError", "Please enter the Recipe steps!!");
+        isValid = false;
+        }
+if(isValid){
+
+const isDuplicate = recipes.some((recipe) => recipe.title.toLowerCase() === recipeTitle.toLowerCase());
+
+if(isDuplicate){
+    alert('The recipe already exist!!!')
+ }else{
+    const newRecipe = {
+        title: recipeTitle,
+        ingredients: recipeIngredients,
+        steps: recipeSteps
+    }
+    recipes.push(newRecipe)
+    
+    document.getElementById("recipeTitle").value= "";
+    
+    document.getElementById("recipeIngredients").value= "";
+    document.getElementById("recipeSteps").value= "";
+
+    saveRecipeToLocalStorage();
+    if (document.querySelector('#recipeList')){  
+    displayRecipes();  
+    }
+  }  
+ }      
+}  
+
+const editRecipe = (index) => {
+
+    document.getElementById(`titleDisplay-${index}`).classList.add("hidden");
+    document.getElementById(`ingredientsDisplay-${index}`).classList.add("hidden");
+    document.getElementById(`stepsDisplay-${index}`).classList.add("hidden");
+
+    document.getElementById(`editBtn-${index}`).classList.add("hidden");
+    document.getElementById(`deleteBtn-${index}`).classList.add("hidden");
+
+    document.getElementById(`titleInput-${index}`).classList.remove("hidden");
+    document.getElementById(`ingredientsInput-${index}`).classList.remove("hidden");
+    document.getElementById(`stepsInput-${index}`).classList.remove("hidden");
+
+    document.getElementById(`saveBtn-${index}`).classList.remove("hidden");
+    document.getElementById(`cancelBtn-${index}`).classList.remove("hidden");
+    
+}
+
+const saveRecipe = (index) => {
+    const updatedRecipeTitle = document.getElementById(`titleInput-${index}`).value.trim();
+    const updatedRecipeIngredients = document.getElementById(`ingredientsInput-${index}`).value.trim();
+    const updatedRecipeSteps = document.getElementById(`stepsInput-${index}`).value.trim();
+
+
+    if(updatedRecipeTitle && updatedRecipeIngredients && updatedRecipeSteps){
+        recipes[index].title = updatedRecipeTitle;
+        recipes[index].ingredients = updatedRecipeIngredients;
+        recipes[index].steps = updatedRecipeSteps;
+        saveRecipeToLocalStorage();  
+        displayRecipes();
+        
+}else{
+    alert('Please edit any field');
+}
+}
+const cancelEdit = (index) => {
+    document.getElementById(`titleDisplay-${index}`).classList.remove("hidden");
+    document.getElementById(`ingredientsDisplay-${index}`).classList.remove("hidden");
+    document.getElementById(`stepsDisplay-${index}`).classList.remove("hidden");
+
+    document.getElementById(`editBtn-${index}`).classList.remove("hidden");
+    document.getElementById(`deleteBtn-${index}`).classList.remove("hidden");
+
+    document.getElementById(`titleInput-${index}`).classList.add("hidden");
+    document.getElementById(`ingredientsInput-${index}`).classList.add("hidden");
+    document.getElementById(`stepsInput-${index}`).classList.add("hidden");
+
+    document.getElementById(`saveBtn-${index}`).classList.add("hidden");
+    document.getElementById(`cancelBtn-${index}`).classList.add("hidden");
+    
+}
+
+const deleteRecipe = (index) => {
+   const deletedRecipe = recipes.splice(index, 1);
+   oldRecipes.push(deletedRecipe);
+    saveRecipeToLocalStorage();
+    if (document.querySelector('#recipeList')){   
+    displayRecipes();
+}
+}
+
+const recipeForm = document.getElementById("recipeForm")
+
+if(recipeForm){
+    document.getElementById('recipeForm').addEventListener('submit', addRecipe);
+}
+
+loadRecipesFromLocalStorage();
+displayRecipes();
---
+++ version_1/scripts.js
@@ -0,0 +1,78 @@
+const recipes = [
+    {
+        title:"Spaghetti Bolognese",
+        ingredients: "Spaghetti, Ground Beef, Tomato Sauce, Parmesan Cheese",
+        steps: "Cook spaghetti, cook ground beef, combine with tomato sauce, top with parmes"
+    },
+    {
+        title:"Chicken Fajitas",
+        ingredients: "Chicken, Bell Peppers, Onions, Tortillas, Salsa",
+        steps: "Cook chicken, cook bell peppers and onions, serve with tortillas and salsa"
+    },
+    {
+        title:"Grilled Cheese Sandwich",
+        ingredients: "Bread, Cheese, Butter",
+        steps: "Butter bread, place cheese in between, grill until cheese is melted"
+    }
+    // },
+    // {
+    //     title:"Fried Rice",
+    //     ingredients: "Cooked Rice, Scrambled Eggs, Vegetables, Soy Sauce",
+    //     steps: "Cook scrambled eggs, add vegetables and soy sauce, combine with cooked rice"
+    //     },
+        
+
+
+];
+
+const displayRecipes = () => {
+    const recipeList = document.querySelector('#recipeList')
+    recipeList.innerHTML = "";
+    // recipeList.textContent = "Hello terminal"
+
+    recipes.forEach((recipe) => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add("bg-white", "p-4", "rounded", "shadow", "mb-4")
+        recipeCard.innerHTML = `
+        <h2 class = "text-lg font-bold">${recipe.title}</h2>
+        <p class = "text-sm text-gray-500"><strong class = "font-bold text-lg">Ingredients: &ensp;</strong>${recipe.ingredients}</p>
+         <p class = "text-sm text-gray-500"><strong class = "font-bold text-lg">Steps: &ensp;</strong>${recipe.steps}</p>
+        `;
+        recipeList.appendChild(recipeCard);
+    })
+}
+const addRecipe = () => {
+    const  recipeTitleInput = document.querySelector('#recipeTitle');
+    const  recipeIngredientsInput = document.querySelector('#recipeIngredients');
+    const  recipeStepsInput = document.querySelector('#recipeSteps');
+
+    const recipeTitle = recipeTitleInput.value.trim();
+    const recipeIngredients = recipeIngredientsInput.value.trim();
+    const recipeSteps = recipeStepsInput.value.trim();
+
+    if(recipeTitle.trim() !== "" && recipeIngredients !== "" && recipeSteps.trim() !== ""){
+const newRecipe = {
+    title: recipeTitle,
+    ingredients: recipeIngredients,
+    steps: recipeSteps
+}
+recipes.push(newRecipe)
+
+recipeTitleInput.value = "";
+
+recipeIngredientsInput.value = "";
+recipeStepsInput.value = "";
+
+displayRecipes();
+
+    }
+    else{
+        alert('Please all fields are required')
+    }
+}
+
+// const addRecipeBtn = document.querySelector('#addRecipeBtn')
+// addRecipeBtn.addEventListener('click' , addRecipe);
+
+document.querySelector("addRecipe").addEventListener("click", addRecipe);
+displayRecipes();
